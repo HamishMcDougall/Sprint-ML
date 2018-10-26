e943121636c40025a13624784159758e2c5bf4c7
hamishmcdougall <hamishmcdougall@gmail.com>
2018-10-26 13:58:55 +1100

    First commit

---
+++ components/footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Created for ML sprint 
+    </p>
+  </div>
+</footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <navbar/>
+      <div class="container navbaroffset">
+              <div class="columns">
+          <div class="column">
+            First column
+          </div>
+          <div class="column">
+            Second column
+          </div>
+          <div class="column">
+            Third column
+          </div>
+          <div class="column">
+            Fourth column
+          </div>
+    </div>
+      </div>
+    <pageFooter/>
+  </div>
+</template>
+
+<script>
+  import navbar from '~/components/navbar.vue'
+  import pageFooter from '~/components/footer.vue'
+  
+  export default {
+    components: {
+      navbar,
+      pageFooter
+    }
+  }
+</script>
+
+<style>
+.navbaroffset{
+  padding-top: 52px;
+}
+</style>
